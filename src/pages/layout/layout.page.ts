053d700365dad6f87cd998a793e3dd7b1d8e91b0
dragunovlab <dragunov.lab@gmail.com>
2025-01-05 20:03:45 +0500

    Updated the CSS in the header component and added the banner component

--- src/pages/layout/layout.page.ts
+++ src/pages/layout/layout.page.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from '../../components/header/header.component';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from '../../components/footer/footer.component';
+import { BannerComponent } from '../../components/banner/banner.component';
 
 @Component({
     selector: 'app-layout',
     standalone: true,
-    imports: [CommonModule, HeaderComponent, RouterOutlet, FooterComponent],
+    imports: [CommonModule, HeaderComponent, BannerComponent, RouterOutlet, FooterComponent],
     templateUrl: './layout.page.html',
     styleUrl: './layout.page.scss'
 })
